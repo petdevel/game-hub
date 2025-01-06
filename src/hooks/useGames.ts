edad97e14b9d8afc2f64b9c95f4757c0fa7cffa1
Peter <petsdeveloper@gmail.com>
2025-01-06 06:20:59 +0100

    implementing infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,31 +6,32 @@ import { Platform } from "./usePlatforms";
 const apiClient = new APIClient<Game>("/games")
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-    rating_top: number
-  }
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number
+}
+
 const useGames = (
   gameQuery: GameQuery
 ) => useInfiniteQuery<FetchResponse<Game>, Error>({
   queryKey: ["games", gameQuery],
-  queryFn: ({ pageParam = 1}) => apiClient.getAll({
-      params: { 
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-        page: pageParam 
-      }
-    }),
-    getNextPageParam: (lastPage, allPages) => {
-      return lastPage.next ? allPages.length + 1 : undefined
-    },
-    initialPageParam: 1
+  queryFn: ({ pageParam = 1 }) => apiClient.getAll({
+    params: {
+      genres: gameQuery.genre?.id,
+      parent_platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder,
+      search: gameQuery.searchText,
+      page: pageParam
+    }
+  }),
+  getNextPageParam: (lastPage, allPages) => {
+    return lastPage.next ? allPages.length + 1 : undefined
+  },
+  initialPageParam: 1,
+  staleTime: 24 * 60 * 60 * 1000 //24H
 })
 
 export default useGames;
